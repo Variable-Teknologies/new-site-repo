e3ce5592c3d9ec94e50e7fae892e118d92db1ce2
unknown <sallazar024@gmail.com>
2019-10-13 21:55:36 -0400

    pushing content

---
+++ src/js/app.js
@@ -0,0 +1,4 @@
+/* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
+particlesJS.load('particles-js', 'particles/particle-cfg.json', function() {
+    console.log('callback - particles.js config loaded');
+  });
